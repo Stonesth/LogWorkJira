12ef2506a4062ec2129fd40dbf1da900ffbfc6c0
Stonesth <pierre.thonon@gmail.com>
2024-12-12 10:28:23 +0100

    Refactor logworkjira2.py to streamline data extraction and logging process for Jira tasks

--- logworkjira2.py
+++ logworkjira2.py
@@ -89,143 +89,151 @@ task_search_input.send_keys(j.jira)
 task_search_input.send_keys(Keys.RETURN)
 
 # Localiser le tableau
-tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="search-results-table"]')
-table = tools.driver.find_element(By.XPATH, '//*[@id="search-results-table"]')
+if tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="search-results-table"]') :
+    table = tools.driver.find_element(By.XPATH, '//*[@id="search-results-table"]')
 
-# Parcourir les lignes du tableau
-rows = table.find_elements(By.XPATH, './/tbody/tr')
+    # Parcourir les lignes du tableau
+    rows = table.find_elements(By.XPATH, './/tbody/tr')
 
 
-# Need to store data 
-array = [ ]
+    # Need to store data 
+    array = [ ]
 
-# Extraire les informations de chaque ligne
-for row in rows:
-    date = row.find_element(By.XPATH, './/td[contains(@id, "date-cell")]').text
-    duration = row.find_element(By.XPATH, './/td[contains(@id, "duration-cell")]').text
-    print(f"Date: {date}, Duration: {duration}")
+    # Extraire les informations de chaque ligne
+    for row in rows:
+        date = row.find_element(By.XPATH, './/td[contains(@id, "date-cell")]').text
+        duration = row.find_element(By.XPATH, './/td[contains(@id, "duration-cell")]').text
+        print(f"Date: {date}, Duration: {duration}")
 
-    # I would like to save all those information in arrays
-    array.append([date, duration])
+        # I would like to save all those information in arrays
+        array.append([date, duration])
 
-print(array)
+    print(array)
 
-# Need to go to the Jira
-j.connectToJira(j.jira)
+    # Need to go to the Jira
+    j.connectToJira(j.jira)
 
-# Need to add all the time for this JIRA
-time_all = timedelta(hours=0, minutes=0, seconds=0)
+    # Need to add all the time for this JIRA
+    time_all = timedelta(hours=0, minutes=0, seconds=0)
 
-# Print the array with all the info for this JIRA
-print("----------------- Infos -----------------  ")
+    # Print the array with all the info for this JIRA
+    print("----------------- Infos -----------------  ")
 
-for r in array:
-    print( ' '.join([str(x) for x in r] ) )
+    for r in array:
+        print( ' '.join([str(x) for x in r] ) )
 
-    # Convertir la chaîne de temps en objet timedelta
-    duration_str = r[1]
-    time_parts = duration_str.split()
-    time_kwargs = {}
-    for part in time_parts:
-        if 'h' in part:
-            time_kwargs['hours'] = int(part.replace('h', ''))
-        elif 'm' in part:
-            time_kwargs['minutes'] = int(part.replace('m', ''))
-        elif 's' in part:
-            time_kwargs['seconds'] = int(part.replace('s', ''))
+        # Convertir la chaîne de temps en objet timedelta
+        duration_str = r[1]
+        time_parts = duration_str.split()
+        time_kwargs = {}
+        for part in time_parts:
+            if 'h' in part:
+                time_kwargs['hours'] = int(part.replace('h', ''))
+            elif 'm' in part:
+                time_kwargs['minutes'] = int(part.replace('m', ''))
+            elif 's' in part:
+                time_kwargs['seconds'] = int(part.replace('s', ''))
 
-    time_object = timedelta(**time_kwargs)
+        time_object = timedelta(**time_kwargs)
 
-    # Extraire les heures, minutes et secondes de l'objet timedelta
-    total_seconds = int(time_object.total_seconds())
-    hours, remainder = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
+        # Extraire les heures, minutes et secondes de l'objet timedelta
+        total_seconds = int(time_object.total_seconds())
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
 
-    print(f"{hours}h {minutes}m {seconds}s")
+        print(f"{hours}h {minutes}m {seconds}s")
 
-    # Need to add the time past to this JIRA
-    time_all += time_object
+        # Need to add the time past to this JIRA
+        time_all += time_object
 
-    # Click on the more button to add the hours 
-    tools.waitLoadingPageByXPATH2(10, '//*[@id="opsbar-operations_more"]')
-    more_button = tools.driver.find_element(By.XPATH, '//*[@id="opsbar-operations_more"]')
-    more_button.click()
+        time.sleep(1)
+        # Click on the more button to add the hours 
+        tools.waitLoadingPageByXPATH2(10, '//*[@id="opsbar-operations_more"]')
+        more_button = tools.driver.find_element(By.XPATH, '//*[@id="opsbar-operations_more"]')
+        more_button.click()
+        time.sleep(1)
 
-    # Click on the log swork button to add the hours 
-    tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work"]/a/span')
-    log_work_button = tools.driver.find_element(By.XPATH, '//*[@id="log-work"]/a/span')
-    log_work_button.click()
+        # Click on the log swork button to add the hours 
+        tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work"]/a/span')
+        log_work_button = tools.driver.find_element(By.XPATH, '//*[@id="log-work"]/a/span')
+        log_work_button.click()
 
-    # Introduce into the input of Time Spent the time for this day
-    tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work-time-logged"]')
-    log_work_time_logged = tools.driver.find_element(By.XPATH, '//*[@id="log-work-time-logged"]')
-    if (hours > 0) :
-        log_work_time_logged.send_keys(str(hours) + 'h ')
-    if (seconds > 29) :
-        log_work_time_logged.send_keys(str(minutes + 1) + 'm ')
-    else :
-        if (minutes > 0) :
-            log_work_time_logged.send_keys(str(minutes) + 'm ')
-        else :
+        # Introduce into the input of Time Spent the time for this day
+        tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work-time-logged"]')
+        log_work_time_logged = tools.driver.find_element(By.XPATH, '//*[@id="log-work-time-logged"]')
+        if (hours > 0) :
+            log_work_time_logged.send_keys(str(hours) + 'h ')
+        if (seconds > 29) :
             log_work_time_logged.send_keys(str(minutes + 1) + 'm ')
+        else :
+            if (minutes > 0) :
+                log_work_time_logged.send_keys(str(minutes) + 'm ')
+            else :
+                log_work_time_logged.send_keys(str(minutes + 1) + 'm ')
 
-    # Introduce into the input the date
-    tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work-date-logged-date-picker"]')
-    log_work_date_logged_date_picker = tools.driver.find_element(By.XPATH, '//*[@id="log-work-date-logged-date-picker"]')
-    log_work_date_logged_date_picker.send_keys(Keys.CONTROL, 'a')
+        # Introduce into the input the date
+        tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work-date-logged-date-picker"]')
+        log_work_date_logged_date_picker = tools.driver.find_element(By.XPATH, '//*[@id="log-work-date-logged-date-picker"]')
+        log_work_date_logged_date_picker.send_keys(Keys.CONTROL, 'a')
 
-    date_object = datetime.strptime(r[0], '%d/%m/%Y')
+        date_object = datetime.strptime(r[0], '%d/%m/%Y')
 
-    # Month
-    month_number = str(date_object.month)
-    datetime_object_month = datetime.strptime(month_number, "%m")
-    month_name = datetime_object_month.strftime("%b")
+        # Month
+        month_number = str(date_object.month)
+        datetime_object_month = datetime.strptime(month_number, "%m")
+        month_name = datetime_object_month.strftime("%b")
+        
+        # Year
+        year_2_digit = date_object.year % 100
+        print(year_2_digit)
+
+        log_work_date_logged_date_picker.send_keys(str(date_object.day) + '/' +month_name+ '/'+str(year_2_digit)+' 10:00 PM')
+
+        tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work-submit"]')
+        log_button = tools.driver.find_element(By.XPATH, '//*[@id="log-work-submit"]')
+        log_button.click()
+
+        
+        
+    print(f"Total time: {time_all}")
+
+    print(str(time_all))
+    timedelta_8 = timedelta(hours=8, minutes=0, seconds=0)
+
+    time_all_sec = time_all.total_seconds()
+    timedelta_8_sec = timedelta_8.total_seconds()
+    total_sec = time_all_sec / timedelta_8_sec
+    print(str(round(total_sec, 3)))
+
+    # Need to update the JIRA for the Story Points only if this one is not filled
+    if tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="customfield_10004-val"]') :
+        print ("There is already a Story Point => don't update the Story Point ")
+    else : 
+        # click on the edit button
+        tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="opsbar-edit-issue_container"]')
+        edit_button = tools.driver.find_element(By.XPATH, '//*[@id="opsbar-edit-issue_container"]')
+        edit_button.click()
+
+        # click in the input field Story Points
+        tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="customfield_10004"]')
+        inputStoryPoint = tools.driver.find_element(By.XPATH, '//*[@id="customfield_10004"]')
+        time.sleep(1)
+        inputStoryPoint.click()
+        time.sleep(1)
+        inputStoryPoint.send_keys(str(round(total_sec, 3)))
+
+        # cllick in the update button
+        tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="edit-issue-submit"]')
+        update_button = tools.driver.find_element(By.XPATH, '//*[@id="edit-issue-submit"]')
+        update_button.click()
+        
+        time.sleep(delay_properties)
+else :
+    print ("No data for this JIRA")
     
-    # Year
-    year_2_digit = date_object.year % 100
-    print(year_2_digit)
-
-    log_work_date_logged_date_picker.send_keys(str(date_object.day) + '/' +month_name+ '/'+str(year_2_digit)+' 10:00 PM')
-
-    tools.waitLoadingPageByXPATH2(10, '//*[@id="log-work-submit"]')
-    log_button = tools.driver.find_element(By.XPATH, '//*[@id="log-work-submit"]')
-    log_button.click()
-
-    
-    
-print(f"Total time: {time_all}")
-
-print(str(time_all))
-timedelta_8 = timedelta(hours=8, minutes=0, seconds=0)
-
-time_all_sec = time_all.total_seconds()
-timedelta_8_sec = timedelta_8.total_seconds()
-total_sec = time_all_sec / timedelta_8_sec
-print(str(round(total_sec, 3)))
-
-# Need to update the JIRA for the Story Points only if this one is not filled
-if tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="customfield_10004-val"]') :
-    print ("There is already a Story Point => don't update the Story Point ")
-else : 
-    # click on the edit button
-    tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="opsbar-edit-issue_container"]')
-    edit_button = tools.driver.find_element(By.XPATH, '//*[@id="opsbar-edit-issue_container"]')
-    edit_button.click()
-
-    # click in the input field Story Points
-    tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="customfield_10004"]')
-    inputStoryPoint = tools.driver.find_element(By.XPATH, '//*[@id="customfield_10004"]')
-    time.sleep(1)
-    inputStoryPoint.click()
-    time.sleep(1)
-    inputStoryPoint.send_keys(str(round(total_sec, 3)))
-
-    # cllick in the update button
-    tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="edit-issue-submit"]')
-    update_button = tools.driver.find_element(By.XPATH, '//*[@id="edit-issue-submit"]')
-    update_button.click()
-    
-    time.sleep(delay_properties)
+    # Need to go to the Jira
+    j.connectToJira(j.jira)
+    total_sec = 0
 
 # Need to place the JIRA in DONE
 tools.waitLoadingPageByXPATH2(delay_properties, '//*[@id="opsbar-transitions_more"]')
